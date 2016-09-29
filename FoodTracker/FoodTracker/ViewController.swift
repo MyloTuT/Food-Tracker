0bf2de05d992eadfc3ac530b69e6342317664db7
Myles Rayfield <myles.rayfield@icloud.com>
2016-09-29 02:57:22 -0400

    Initial Commit

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Myles Rayfield on 9/29/16.
+//  Copyright © 2016 Myles Rayfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
